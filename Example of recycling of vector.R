4a29b7e9cf1a6ec3090405c3d98ba58292c91666
rdolia <43548125+rdolia@users.noreply.github.com>
2018-11-23 22:06:24 +0530

    Add files via upload

---
+++ Example of recycling of vector.R
@@ -0,0 +1,11 @@
+
+#Example of recycling of vector.
+
+vector1 <-c(1,3,6)
+vector2 <-c(10,20,30,40,50) 
+
+vector3 <- vector1 + vector2
+
+vector3
+# for vector1 values 1 and 3 are repeated hence we have 
+#vector3 as (1+10),(3+20),(6+30),(1+40),(3+50) Hence 1 and 3 are repeated from shorter vector. 
